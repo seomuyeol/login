3b4a626686eda16fec85128afa38ed8b9d6e2610
seomy <muyear.seo@gmail.com>
2022-06-03 13:42:55 +0900

    login

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,8 +1,9 @@
 package hello.login.web;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
@@ -10,6 +11,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "redirect:/items";
+        return "home";
     }
 }
