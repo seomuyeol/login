7b2742824ed532660757046bb6a565b1797f2f80
seomy <muyear.seo@gmail.com>
2022-06-03 16:59:06 +0900

    session

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -1,5 +1,7 @@
 package hello.login.web.login;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -26,7 +28,7 @@ public class LoginController {
 	}
 
 	@PostMapping("/login")
-	public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+	public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
 		if (bindingResult.hasErrors()) {
 			return "login/loginForm";
 		}
@@ -36,7 +38,24 @@ public class LoginController {
 			bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
 			return "login/loginForm";
 		}
-		// 로그인 성공 처리 TODO
+		// 로그인 성공 처리 
+		
+		//쿠키 시간정보를 주지 않으면 세션 쿠키 (브라우저 종료 시 )
+		Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+		response.addCookie(idCookie);
+		
 		return "redirect:/";
+	}
+	
+	@PostMapping("/logout")
+	public String logout(HttpServletResponse response) {
+		expireCookie(response, "memberId");
+		return "redirect:/";
+	}
+
+	private void expireCookie(HttpServletResponse response, String cookieName) {
+		Cookie cookie = new Cookie(cookieName, null);
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
 	}
 }
